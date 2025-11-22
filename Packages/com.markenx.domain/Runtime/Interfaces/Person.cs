8a404a015b05a05396dc873c843763e3fba12232
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-22 18:17:36 -0500

    feat(Person): Se aplica readonly
    Para evitar que los atributos _firstname y _lastName puedan modificarse

--- Packages/com.markenx.domain/Runtime/Interfaces/Person.cs
+++ Packages/com.markenx.domain/Runtime/Interfaces/Person.cs
@@ -1,7 +1,7 @@
 public abstract class Person
 {
-  private string _firstName;
-  private string _lastName;
+  private readonly string _firstName;
+  private readonly string _lastName;
 
   public string FirstName { get { return _firstName; } }
   public string LastName { get { return _lastName; } }
