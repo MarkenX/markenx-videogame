a32f46ff80d4314639141789dc8b4e894b598053
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-23 00:39:09 -0500

    feat(Consumer): Se agregan intereses

--- Packages/com.markenx.domain/Runtime/Models/Consumer.cs
+++ Packages/com.markenx.domain/Runtime/Models/Consumer.cs
@@ -1,21 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 public class Consumer
 {
   private readonly NationalIdentityDocument _nationalIdentityDocument;
+  private readonly HashSet<ProductCategory> _interests;
+  private readonly decimal _monthlyIncome;
 
-  public Consumer(NationalIdentityDocument nationalIdentityDocument, decimal monthlyIncome)
+  public Consumer(
+    NationalIdentityDocument nationalID,
+    IEnumerable<ProductCategory> interests,
+    decimal monthlyIncome)
   {
-    _nationalIdentityDocument = nationalIdentityDocument
-        ?? throw new ArgumentNullException(nameof(nationalIdentityDocument));
-
-    MonthlyIncome = monthlyIncome;
+    _nationalIdentityDocument = RequireNationalIdentityDocument(nationalID);
+    _interests = new HashSet<ProductCategory>(interests);
+    _monthlyIncome = monthlyIncome;
   }
 
   public string FirstName => _nationalIdentityDocument.FirstName;
   public string LastName => _nationalIdentityDocument.LastName;
+  public IReadOnlyCollection<ProductCategory> Interests => _interests;
+  public decimal MonthlyIncome => _monthlyIncome;
   public int Age => CalculateAge(_nationalIdentityDocument.BirthDate);
-  public decimal MonthlyIncome { get; }
+
+  private NationalIdentityDocument RequireNationalIdentityDocument(NationalIdentityDocument nationalID)
+  {
+    return nationalID ?? throw new ArgumentNullException(nameof(nationalID));
+  }
 
   private int CalculateAge(DateTime birthDate)
   {
