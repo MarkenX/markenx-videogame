34c6e639a6c92e7b8e1dadc3bf7aa1e84c05e6e7
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-22 23:40:11 -0500

    feat(Consumer): Se agrega calculo para obtener la edad

--- Packages/com.markenx.domain/Runtime/Models/Consumer.cs
+++ Packages/com.markenx.domain/Runtime/Models/Consumer.cs
@@ -1,9 +1,28 @@
 public class Consumer
 {
   private readonly NationalIdentityDocument _nationalIdentityDocument;
-  private readonly decimal _monthlyIncome;
 
-  public string FirstName { get { return _nationalIdentityDocument.FirstName; } }
+  public Consumer(NationalIdentityDocument nationalIdentityDocument, decimal monthlyIncome)
+  {
+    _nationalIdentityDocument = nationalIdentityDocument
+        ?? throw new ArgumentNullException(nameof(nationalIdentityDocument));
 
-  public decimal MonthlyIncome { get { return _monthlyIncome; } }
+    MonthlyIncome = monthlyIncome;
+  }
+
+  public string FirstName => _nationalIdentityDocument.FirstName;
+  public string LastName => _nationalIdentityDocument.LastName;
+  public int Age => CalculateAge(_nationalIdentityDocument.BirthDate);
+  public decimal MonthlyIncome { get; }
+
+  private int CalculateAge(DateTime birthDate)
+  {
+    var today = DateTime.Today;
+    int age = today.Year - birthDate.Year;
+
+    if (birthDate.Date > today.AddYears(-age))
+      age--;
+
+    return age;
+  }
 }
