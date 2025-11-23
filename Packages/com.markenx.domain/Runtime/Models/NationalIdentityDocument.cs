a93856cd27ce1a9a1d70c3e60214b219402974b3
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-23 15:11:13 -0500

    feat(NID): Se convierte la clase en record

--- Packages/com.markenx.domain/Runtime/Models/NationalIdentityDocument.cs
+++ Packages/com.markenx.domain/Runtime/Models/NationalIdentityDocument.cs
@@ -1,12 +1,7 @@
 using System;
 
-public class NationalIdentityDocument
-{
-  private readonly string _firstName;
-  private readonly string _lastName;
-  private readonly DateTime _birthDate;
-
-  public string FirstName { get { return _firstName; } }
-  public string LastName { get { return _lastName; } }
-  public DateTime BirthDate { get { return _birthDate; } }
-}
+public record NationalIdentityDocument(
+    string FirstName,
+    string LastName,
+    DateTime BirthDate
+);
