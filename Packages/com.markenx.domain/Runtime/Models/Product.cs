9852452f2fc343dd295ad485d4d56c328695f5e9
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-11-23 00:41:53 -0500

    feat(Product): Se agrega categoría

--- Packages/com.markenx.domain/Runtime/Models/Product.cs
+++ Packages/com.markenx.domain/Runtime/Models/Product.cs
@@ -1,11 +1,14 @@
 public class Product
 {
+  private readonly ProductCategory _category;
   private readonly float _price;
 
-  public Product(float price)
+  public Product(ProductCategory category, float price)
   {
+    _category = category;
     _price = price;
   }
 
-  public float Price { get { return _price; } }
+  public ProductCategory Category => _category;
+  public float Price => _price;
 }
