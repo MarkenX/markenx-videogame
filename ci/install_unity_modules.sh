bca2f9bd799e6af30e4ca7ecc1926ac8027de436
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-12-01 00:24:26 -0500

    Remove obsolete Dockerfile and Unity module installation script for WebGL

--- ci/install_unity_modules.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-UNITY_VERSION=$1
-
-echo "Installing Unity modules for version $UNITY_VERSION"
-
-unityhub install --version "$UNITY_VERSION" --changeset d3d30d158480 --childModules WebGL
